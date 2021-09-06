ba8e87a6a5447fabe8f49c6387a8585a4ecc47f5
panictattoo <danielnahshoni@gmail.com>
2021-09-06 16:58:35 +0300

    added button for adding items and handling errors(404/500),
    Co-authored-by: Tomer Ramon <tomer.ramon1@gmail.com>
    Co-authored-by: Daniel Nachshoni <danielnahshoni@gmail.com>

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname,'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/orders', require('./routes/orders'))
 
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
---
+++ models/orders.js
@@ -0,0 +1,39 @@
+// orders.js = Story.js
+
+const mongoose = require('mongoose')
+
+const OrdersSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    body: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: String,
+        default: 'public',
+        enum: ['public','private']
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    image: {
+        type: String,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('Order', OrdersSchema)
