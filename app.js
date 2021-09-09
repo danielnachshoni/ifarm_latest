55e02e1f53b5514e90d035ce270cae6f0489b931
Tomer Ramon <tomer.ramon1@gmail.com>
2021-09-09 16:17:42 +0300

    Eslint config changes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const MongoStore = require('connect-mongo')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const connectDB = require('./config/db')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const passport = require('passport')
-const session = require('express-session')
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const express = require("express")
+// const mongoose = require("mongoose")
+const MongoStore = require("connect-mongo")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const connectDB = require("./config/db")
+const exphbs = require("express-handlebars")
+const path = require("path")
+const passport = require("passport")
+const session = require("express-session")
 
 //load config
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: "./config/config.env" })
 
 //passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport)
 
 connectDB()
 
@@ -24,25 +26,25 @@ app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
 // HandleBars Helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require("./helpers/hbs")
 
 // Handlebars
-app.engine('.hbs',exphbs({helpers: { 
-    formatDate,
-    stripTags, 
-    truncate,
-    editIcon},
-    defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(".hbs",exphbs({helpers: { 
+	formatDate,
+	stripTags, 
+	truncate,
+	editIcon},
+defaultLayout: "main", extname: ".hbs"}))
+app.set("view engine", ".hbs")
 
 
 // SESSION
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI})  //(URI FROM.env file)
+	secret: "keyboard cat",
+	resave: false,
+	saveUninitialized: false,
+	store: MongoStore.create({
+		mongoUrl: process.env.MONGO_URI})  //(URI FROM.env file)
 }))
 
 // PASSPORT MIDDLEWARE
@@ -51,25 +53,25 @@ app.use(passport.session())
 
 // Global Variable
 app.use(function(req, res, next){
-    res.locals.user = req.user || null
-    next()
+	res.locals.user = req.user || null
+	next()
 })
 
 //STATIC FOLDER
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,"public")))
 
 // Routes
-app.use('/', require('./routes/index'))
-app.use('/auth', require('./routes/auth'))
-app.use('/orders', require('./routes/orders'))
+app.use("/", require("./routes/index"))
+app.use("/auth", require("./routes/auth"))
+app.use("/orders", require("./routes/orders"))
 
-if(process.env.NODE_ENV==='development'){
-    app.use(morgan('dev'))
+if(process.env.NODE_ENV==="development"){
+	app.use(morgan("dev"))
 }
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
 
