0268c9a08ee446da3d27ff5b40576e9b89405805
panictattoo <danielnahshoni@gmail.com>
2021-09-11 18:15:26 +0300

    minor frontend changes. added 'add to cart' button,
    Co-authored-by: Tomer Ramon <tomer.ramon1@gmail.com>
    Co-authored-by: Daniel Nachshoni <danielnahshoni@gmail.com>
    Co-authored-by: Eli Speyer <elispaier@gmail.com>

--- models/User.js
+++ models/User.js
@@ -28,7 +28,12 @@ const UserSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    Cart: {
+        type: Array,
+        required: true
+
+    },
 
 })
 
